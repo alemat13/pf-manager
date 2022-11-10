143b71791963de76017417351857c0b57f2812be
Alex <alemat13@gmail.com>
2022-11-10 23:05:10 +0100

    Maven init

---
+++ core/src/main/java/com/pfmanager/App.java
@@ -0,0 +1,13 @@
+package com.pfmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
