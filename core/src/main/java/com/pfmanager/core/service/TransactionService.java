02b58fb0ecd98918226c1c59411dc277714080f5
Alexandre Mathiaud <alexandre.mathiaud@realestate.bnpparibas>
2022-11-16 17:44:42 +0100

    Optimizations

--- core/src/main/java/com/pfmanager/core/service/TransactionService.java
+++ core/src/main/java/com/pfmanager/core/service/TransactionService.java
@@ -5,24 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pfmanager.core.entity.transaction.TransactionMapping;
 import com.pfmanager.core.entity.transaction.Transaction;
 import com.pfmanager.core.repository.transaction.TransactionMappingRepository;
 import com.pfmanager.core.repository.transaction.TransactionRepository;
 
+@Service
 public class TransactionService {
     private @Autowired TransactionRepository transactionRepository;
     private @Autowired TransactionMappingRepository transactionMappingRepository;
+
     public Transaction createTransaction(Transaction transaction) {
         return this.transactionRepository.save(transaction);
     }
+
     public Iterable<Transaction> findAllTransactions() {
         return this.transactionRepository.findAll();
     }
+
     private Double getTotalAmount(List<Transaction> transactions) {
         return transactions.stream().map(t -> t.getAmount()).reduce(0.0, (a, b) -> a + b);
     }
+
     private Iterable<TransactionMapping> createTransactionMappings(List<Transaction> sourceTransactions, List<Transaction> targetTransactions) {
         if(sourceTransactions.stream().filter(t -> ! t.getActive()).count() > 0) {
             throw new Error("All source transactions must be active");
@@ -44,40 +50,52 @@ public class TransactionService {
         });
         return this.transactionMappingRepository.saveAll(transactionMappings);
     }
-    public void unsplitTransaction(Transaction sourceTransaction) {
-        List<TransactionMapping> transactionMappings = sourceTransaction.getSourceMappings();
-        if(sourceTransaction.getActive()) {
-            throw new Error("The source transaction must be inactive");
+
+    private void deleteTransactionMapping(Transaction sourceTransaction, Transaction targetTransaction) {
+        List<TransactionMapping> transactionMappings = new ArrayList<>();
+        if(sourceTransaction == null) {
+            if (targetTransaction == null) {
+                throw new Error("One of sourceTransaction/targetTransaction must be provided, none provided");
+            }
+            else {
+                transactionMappings = targetTransaction.getTargetMappings();
+            }
+        }
+        else {
+            if(targetTransaction == null) {
+                transactionMappings = sourceTransaction.getSourceMappings();
+            }
+            else {
+                throw new Error("One of sourceTransaction/targetTransaction must be provided, two provided");
+            }
+        }
+        if(transactionMappings.stream().filter(tm -> tm.getSourceTransaction().getActive()).count() > 0) {
+            throw new Error("All source transactions must be inactive");
         }
         if(transactionMappings.stream().filter(tm -> ! tm.getTargetTransaction().getActive()).count() > 0) {
             throw new Error("All target transactions must be active");
         }
         transactionMappings.forEach(tm -> {
             this.transactionRepository.delete(tm.getTargetTransaction());
-        });
-        this.transactionMappingRepository.deleteAll(transactionMappings);
-        sourceTransaction.setActive(true);
-        this.transactionRepository.save(sourceTransaction);
-    }
-    public void ungroupTransaction(Transaction targetTransaction) {
-        List<TransactionMapping> transactionMappings = targetTransaction.getTargetMappings();
-        if (transactionMappings.stream().filter(tm -> tm.getSourceTransaction().getActive()).count() > 0) {
-            throw new Error("All source transactions must be inactive");
-        }
-        if(!targetTransaction.getActive()) {
-            throw new Error("The target transaction must be active");
-        }
-        this.transactionRepository.delete(targetTransaction);
-        this.transactionMappingRepository.deleteAll(transactionMappings);
-        transactionMappings.forEach(tm -> {
             tm.getSourceTransaction().setActive(true);
             this.transactionRepository.save(tm.getSourceTransaction());
         });
+        this.transactionMappingRepository.deleteAll(transactionMappings);
     }
+
     public Iterable<TransactionMapping> groupTransactions(List<Transaction> sourceTransactions, Transaction targetTransaction) {
         return this.createTransactionMappings(sourceTransactions, Arrays.asList(targetTransaction));
     }
+
+    public void ungroupTransaction(Transaction targetTransaction) {
+        this.deleteTransactionMapping(null, targetTransaction);
+    }
+
     public Iterable<TransactionMapping> splitTransaction(Transaction sourceTransaction, List<Transaction> targetTransactions) {
         return this.createTransactionMappings(Arrays.asList(sourceTransaction), targetTransactions);
+    }
+
+    public void unsplitTransaction(Transaction sourceTransaction) {
+        this.deleteTransactionMapping(sourceTransaction, null);
     }
 }
